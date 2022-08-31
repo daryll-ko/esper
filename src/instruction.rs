15a6eaf6570e42ee4665bcb11c1a4681aaee1d4f
Daryll Ko <daryll.ko@gmail.com>
2022-08-31 12:42:42 +0800

    feat: add `Opcode` enum and `Instruction` struct

---
+++ src/instruction.rs
@@ -0,0 +1,33 @@
+#[derive(Debug, PartialEq)]
+pub enum Opcode {
+    HALT,
+    ILLEGAL,
+}
+
+#[derive(Debug, PartialEq)]
+pub struct Instruction {
+    opcode: Opcode,
+}
+
+impl Instruction {
+    pub fn new(opcode: Opcode) -> Instruction {
+        Instruction { opcode }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_create_halt() {
+        let opcode = Opcode::HALT;
+        assert_eq!(opcode, Opcode::HALT);
+    }
+
+    #[test]
+    fn test_create_instruction() {
+        let instruction = Instruction::new(Opcode::HALT);
+        assert_eq!(instruction.opcode, Opcode::HALT);
+    }
+}
