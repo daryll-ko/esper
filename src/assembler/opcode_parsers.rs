596b5579cdaa724066f5420507c5a6c157c664d8
Daryll Ko <daryll.ko@gmail.com>
2022-09-04 15:19:40 +0800

    fix: make `assembler` tests clear to look at

--- src/assembler/opcode_parsers.rs
+++ src/assembler/opcode_parsers.rs
@@ -15,7 +15,6 @@ mod tests {
     fn test_parse_opcode_load() {
 		let result = opcode_load("load");
         assert_eq!(result.is_ok(), true);
-        
 		let (rest, token) = result.unwrap();
         assert_eq!(token, Token::Op { code: Opcode::LOAD });
         assert_eq!(rest, "");
